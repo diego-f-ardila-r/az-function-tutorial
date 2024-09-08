7a67c6fc366bc16e1bc136d294cbf86ae0aac3dd
Diego Ardila <diego.ardila@providaexternos.cl>
2024-09-07 23:02:51 -0300

    chore: Update DurableTester.cs to add message to storage queue

--- az-function-getname/MessageSender.cs
+++ az-function-getname/MessageSender.cs
@@ -1,29 +1,29 @@
 
-// using System;
-// using System.Net.Http;
-// using System.Text;
-// using Microsoft.Azure.WebJobs;
-// using Microsoft.Azure.WebJobs.Host;
-// using Microsoft.Extensions.Logging;
-// using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
-// namespace az_function_tutorial.Demo
-// {
-//     public class MessageSender
-//     {
-//         [FunctionName("MessageSender")]
-//         public void Run([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer, ILogger log)
-//         {
-//             var message = $"C# Timer trigger function executed at: {DateTime.Now}";
+namespace az_function_tutorial.Demo
+{
+    public class MessageSender
+    {
+        [FunctionName("MessageSender")]
+        public void Run([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            var message = $"C# Timer trigger function executed at: {DateTime.Now}";
 
-//             HttpClient client = new();
-//             HttpRequestMessage requestMessage = new(HttpMethod.Post, "http://localhost:7071/api/MessageReceiver");
+            HttpClient client = new();
+            HttpRequestMessage requestMessage = new(HttpMethod.Post, "https://az-function-tutorial.azurewebsites.net/api/MessageReceiver");
 
-//             requestMessage.Content = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json");
+            requestMessage.Content = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json");
 
-//             client.Send(requestMessage);
+            client.Send(requestMessage);
 
-//             log.LogInformation("Timer Function Executed");
-//         }
-//     }
-// }
+            log.LogInformation("Timer Function Executed");
+        }
+    }
+}
